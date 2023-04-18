642f9d55887cc3826a6a42c8511847524c0a9f03
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-04-18 03:04:07 -0400

    added js files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+// DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// PAGES
+import Edit from "./Pages/Edit";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+import Error from "./Pages/Error";
+
+// COMPONENTS
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/transactions" element={<Index />} />
+            <Route path="/transactions/new" element={<New />} />
+            <Route path="/transactions/:index" element={<Show />} />
+            <Route path="/transactions/:index/edit" element={<Edit />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
