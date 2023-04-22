43585d15bec3b83636b0f611cdc8a6a0103f8736
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-04-21 23:00:40 -0400

    adjusted home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div className="Home" style={{ maxWidth: '500px', margin: '0 auto', paddingTop: '50px' }}>
       <h2>Welcome</h2>
       <h3>To The Budgeting app!</h3>
+      Click<span>&nbsp;</span>
+      <Link to="/transactions">
+        <span>Budgeted Transactions</span>
+      </Link>
+      <span>&nbsp;</span>to begin
       {/* <Login /> */}
     </div>
   );
