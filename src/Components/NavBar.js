212700a7447bf83b22540fa683a2575f4e8a6490
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-04-21 21:13:43 -0400

    image for navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
     <nav className="navbar">
       <div className="app-name">
         <h1>
-          <Link to="/transactions">Budgeting App</Link>
+          <Link to="/transactions">Budgeting App</Link> <img src="https://raw.githubusercontent.com/jkpasaoa/images/main/calculator.jpeg" alt="calculator" style={{ width: "50px", height: "50px" }}></img>
         </h1>
       </div>
       <button className="new-transaction">
